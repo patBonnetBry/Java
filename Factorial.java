d541b2e46baed3bf7cb52278243665849079f5bc
patBonnetBry <patrick.bonnet.bry@sfr.fr>
2017-03-30 21:02:47 +0200

    Make dummy change in Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,34 +1,35 @@
 import java.util.Scanner;
 
-public class Factorial{
+public class Factorial {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		//Prompt user to enter integer
+		// Prompt user to enter integer
 		System.out.print("Enter a non-negative integer: ");
-		
-		//Proceed with factorial calculation only if inputted number is not negative
-		if(input.hasNextInt()){
+
+		// Proceed with factorial calculation only if inputted number is not
+		// negative
+		if (input.hasNextInt()) {
 			int number = input.nextInt();
-			if (number < 0){
+			if (number < 0) {
 				System.out.print("Cannot execute. Please enter a non-negative integer: ");
 				number = input.nextInt();
-		} else {
-			//Output of factorial for any non-negative number
-			System.out.println("The factorial of "+number+" will yield: "+factorial(number));
+			} else {
+				// Output of factorial for any non-negative number
+				System.out.println("The factorial of " + number + " will yield: " + factorial(number));
+			}
 		}
-		}	
 	}
 
-	//Factorial method
-	public static long factorial(int n){
+	// Factorial method
+	public static long factorial(int n) {
 
-		if (n==0){
+		if (n == 0) {
 			return 1;
-		} else if (n==1){
+		} else if (n == 1) {
 			return 1;
 		} else {
-			return n * factorial(n-1);
+			return n * factorial(n - 1);
 		}
 
 	}
